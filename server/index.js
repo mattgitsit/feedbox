5f7c155dbec36217493fb10011d0d1a16f4ad5fb
mattgitsit <39727866+mattgitsit@users.noreply.github.com>
2019-03-04 01:41:15 -0800

    fixing deployment issue

--- server/index.js
+++ server/index.js
@@ -30,12 +30,12 @@ require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets (eg. main.js file or main.css file)
-  app.use(express.static('/client/build'));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   // Express will serve up the index.html file if it does not recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
